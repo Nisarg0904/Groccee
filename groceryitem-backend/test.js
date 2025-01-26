601e94f9e64ab06d46ef689ef600603acc5b23bf
Nisarg Bhatti <nisarg.bhatti0904@gmail.com>
2025-01-26 02:42:52 -0500

    Add .gitignore to ignore node_modules and other files

--- groceryitem-backend/test.js
+++
@@ -1,5 +0,0 @@
-const { validateItem } = require("./utils/apiHelper");
-
-validateItem("Milk")
-  .then((item) => console.log("Validated Item:", item))
-  .catch((err) => console.error("Error:", err.message));
